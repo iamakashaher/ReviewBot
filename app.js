95d414a037d755dd93bec6502bd0a130006839da
Akash Aher <aherakash3000@gmail.com>
2025-06-25 01:21:07 +0530

    Implement bubble sort algorithm and update console output

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello, world!!! This is a sample Node.js program for reviewBot.');
+let numbers = [5, 2, 9, 1, 7];
+
+for (let i = 0; i < numbers.length - 1; i++) {
+  for (let j = 0; j < numbers.length - i - 1; j++) {
+    if (numbers[j] > numbers[j + 1]) {
+      let temp = numbers[j];
+      numbers[j] = numbers[j + 1];
+      numbers[j + 1] = temp;
+    }
+  }
+}
+
+console.log("Sorted list (Bubble Sort):", numbers);
