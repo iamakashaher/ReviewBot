502904382c188e8fece3038dde2400fdee3cf410
Akash <aherakash3000@gmail.com>
2025-07-15 19:39:00 +0530

    Refactor app.js to implement doStuff function and remove outdated console log message

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!!!kkjkjk This is a sample Nodesdkk.js program for reviewBot!');
+// BAD CODE PRACTICE EXAMPLE
+
+function doStuff(a){
+    var x = 0;
+    for(i=0;i<a.length;i++){
+        x+=a[i]
+    }
+    console.log("done")
+    return x
+}
+
+doStuff([1,2,3,4,5]);
