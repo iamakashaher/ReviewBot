d5591f8ce15e814c220e5078cff9ece709cdca70
Akash <aherakash3000@gmail.com>
2025-07-15 20:36:41 +0530

    Rename function doStuff to doSsdasdatuff for clarity and consistency

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // BAD CODE PRACTICE EXAMPLE
 
-function doStuff(a){
-    var x = 0;
-    for(i=0;i<a.length;i++){
-        x+=a[i]
-    }
-    console.log("done")
-    return x
+function doSsdasdatuff(a) {
+  var x = 0;
+  for (i = 0; i < a.length; i++) {
+    x += a[i];
+  }
+  console.log("done");
+  return x;
 }
 
-doStuff([1,2,3,4,5]);
+doSsdasdatuff([1, 2, 3, 4, 5]);
